745bf1fe9b924156dc178c60e1002958d7cf22f0
Eugenio Luo <eugenioluo@gmail.com>
2026-06-25 00:19:59 +0000

    feat: add help and version flag output
    Also clean up ProgramOptions interface

--- src/include/args.hpp
+++ src/include/args.hpp
@@ -23,8 +23,29 @@ public:
     bool help = false;
     bool version = false;
 
-    ProgramOptions(const std::vector<std::string_view>& args) {
+    std::string versionString(void) {
+        return "joltgrep 0.0.1\n";
+    }
+
+    std::string helpString(void) {
         constexpr auto ctx = std::meta::access_context::current();
+        std::string out {"joltgrep usage:\n"};
+
+        template for (constexpr auto member :
+                      std::define_static_array(std::meta::nonstatic_data_members_of(^^ProgramOptions, ctx))) {
+            const auto memStr = std::format("--{0}\n", std::meta::identifier_of(member));
+            out += memStr;
+        }
+
+        return out;
+    }
+
+    ProgramOptions(int argc, char **argv) {
+        constexpr auto ctx = std::meta::access_context::current();
+        const auto args = std::span{argv, argv + argc}
+            | std::views::drop(1)
+            | std::views::transform([](const auto *arg) { return std::string_view{arg}; })
+            | std::ranges::to<std::vector>();
 
         template for (constexpr auto member :
                       std::define_static_array(std::meta::nonstatic_data_members_of(^^ProgramOptions, ctx))) {
--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,22 @@
 #include "args.hpp"
 
-#include <span>
 #include <print>
+
+void version(ProgramOptions& opts) {
+    std::print("{}", opts.versionString());
+}
+
+void help(ProgramOptions& opts) {
+    std::print("{}", opts.helpString());
+}
 
 int main(int argc, char **argv)
 {
-    const auto args = std::span{argv, argv + argc}
-        | std::views::drop(1)
-        | std::views::transform([](const auto *arg) { return std::string_view{arg}; })
-        | std::ranges::to<std::vector>();
-    ProgramOptions opts(args);
+    ProgramOptions opts(argc, argv);
 
-    std::print("help: {0}, version: {1}\n", opts.help, opts.version);
+    if (opts.help) {
+        help(opts);
+    } else if (opts.version) {
+        version(opts);
+    }
 }
